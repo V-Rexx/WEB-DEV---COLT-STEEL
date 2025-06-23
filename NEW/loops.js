ba136619399d4f8e7f38dd772756947087a5445c
V-Rexx <bvigoraj@gmail.com>
2025-06-23 23:37:18 +0530

    Iteration

--- NEW/loops.js
+++ NEW/loops.js
@@ -35,11 +35,67 @@
 
 // }
 
-const SECRET =  "BabyHippo";
+// const SECRET =  "BabyHippo";
 
-let guess = prompt("enter the secret code.....");
-while(guess !== SECRET){
-    guess = prompt("enter the secret code.....");
+// let guess = prompt("enter the secret code.....");
+// while(guess !== SECRET){
+//     guess = prompt("enter the secret code.....");
+// }
+// console.log("congrats you got the secret");
+
+//-----------------------------------------------------
+// For...of loop : nice and easy way to iterate over arrays.
+
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer'];
+
+// for(let i = 0; i<subreddits.length; i++){
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`);
+// }
+
+// for(let sub of subreddits){
+//     console.log(`Visit reddit.com/r/${sub}`);
+// }
+
+
+// const seatingChart = [
+//     ['kristen', 'Erik', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
+
+// for(let row of seatingChart){
+//     for(let students of row){
+//         console.log(students);
+//     }
+// }
+
+//----------------------------------------------
+//Iterate over object literals. however objects are not iterable.
+const testScores = {
+    keeman: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60
 }
-console.log("congrats you got the secret");
 
+for(let person in testScores) {
+    console.log(`${person} scored ${testScores[person]}`);
+}
+
+// Object.keys(testScores) -> returns array of keys of objects
+// Object.values(testScores) -> returns array of values of objects
+// Object.entries(testScores) -> returns nested array of key-value pairs
+
+let total = 0;
+let scores = Object.values(testScores);
+for(let s of scores){
+    total += s;
+}
+console.log(total);
+console.log(`Average is: ${total/scores.length}`);
