044be36be1e553b74ce9511adace490618864fe4
V-Rexx <bvigoraj@gmail.com>
2025-06-30 00:10:38 +0530

    commit

---
+++ NEW/callbacks&arraymethods.js
@@ -0,0 +1,44 @@
+const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+// nums.forEach(function(n){    // Accepts a callback function. Calls the function once per element in the array.
+//     console.log(n*n);
+// });
+
+// function print(el){
+//     console.log(el);
+// }
+
+// nums.forEach(print);
+// nums.forEach(function(el){
+//     console.log(el);
+// });
+
+nums.forEach(function(el){
+    if(el%2===0){
+        console.log(el);
+    }
+});
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+
+]
+
+movies.forEach(function(movie){
+    console.log(`${movie.title} - ${movie.score}/100`);
+});
---
+++ NEW/map.js
@@ -0,0 +1,37 @@
+// Map creates a new array with the results of calling a callback on every element in the array
+const texts = ['rofl', 'lol', 'omg', 'ttyl'];
+const caps = texts.map(function(t){
+    return t.toUpperCase();
+})
+
+const numbers = [1, 2, 3, 4, 5, 6,  7, 8, 9];
+
+const doubles = numbers.map(function(num){
+    return num*2;
+})
+
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+
+]
+
+const titles = movies.map(function(movie){
+    return movie.title.toUpperCase();
+})
+
